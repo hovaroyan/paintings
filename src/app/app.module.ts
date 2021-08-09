0e7d21ce5bc4d183e9e971a68a89631f2e6efc87
hovaroyan <hovaroyan@gmail.com>
2021-08-09 17:49:32 +0400

    added button component + design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { CanvasComponent } from './canvas/canvas.component';
 import { CircleComponent } from './circle/circle.component';
 import {FormsModule} from "@angular/forms";
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CanvasComponent,
-    CircleComponent
+    CircleComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
