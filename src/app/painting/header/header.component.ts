140f31df953d15db1534ea84ef16b2b5d015304d
hovaroyan <hovaroyan@gmail.com>
2021-08-12 01:16:17 +0400

    refactor header

--- src/app/painting/header/header.component.ts
+++ src/app/painting/header/header.component.ts
@@ -10,27 +10,8 @@ import { LocalStorageService } from '../../services/storage.service';
 export class HeaderComponent implements OnInit {
 usersListName = 'usersList';
 email: string = this.userInfo.userEmail;
-name!:string;
   constructor(private storage:LocalStorageService ,private userInfo: UserInfo) { }
 
   ngOnInit(): void {    
   }
-
-  getUserInfo() {
-    const userStr = this.storage.get(this.usersListName)
-    if(userStr){
-      const user = JSON.parse(userStr);
-      return user;
-    }
-  }
-
-  getUserName() {
-    const currentUser = this.getUserInfo();
-    currentUser?.map((info:{ email: string; name: string })=> {      
-        if(this.email === info.email){
-          this.name = info.name
-        }
-    })
-  }
-
 }
