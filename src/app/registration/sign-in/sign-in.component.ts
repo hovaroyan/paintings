0d1c94ec5acc24231699be7fd192280e4ad2645a
hovaroyan <hovaroyan@gmail.com>
2021-08-11 18:49:33 +0400

    sign in functionality

--- src/app/registration/sign-in/sign-in.component.ts
+++ src/app/registration/sign-in/sign-in.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { IUsers } from 'src/app/painting/interfaces/users.interface';
+import { LocalStorageService } from '../../services/storage.service';
 
 @Component({
   selector: 'app-sign-in',
@@ -10,6 +13,10 @@ export class SignInComponent  {
 
   counter = 1;
   index: number = 0;
+  usersList: IUsers[] = [];
+  usersListName = 'users list';
+  checkInputs: boolean = false;
+
 
   items= this.fb.group({
     email: this.fb.control('',[Validators.required]),
@@ -17,12 +24,29 @@ export class SignInComponent  {
   });
   
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private storage: LocalStorageService, private router: Router) { }
 
+getUserInfo() {
+  const userStr = this.storage.get(this.usersListName)
+  if(userStr){
+    const user = JSON.parse(userStr);
+    return user;
+  }
+  
+}
 
 
 handleSignIn() {
-console.log('sign in');
+const signInUser = this.getUserInfo();
+console.log(this.items.valid);
+
+signInUser.map((info: { email: string; password: string })=>{
+  if(info.email === this.items.value.email && info.password === this.items.value.password){
+   this.router.navigate(['/painting'])
+  } else {
+    this.checkInputs = true;
+  }
+});
 
   
 }
